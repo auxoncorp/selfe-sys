ed3957ecc161eb467d64ee176355cf1c963b6bb5
Corey Richardson <corey@octayn.net>
2016-03-02 03:38:09 +1100

    Panic at the end of lang_start
    This is hypothetically more robust than depending on the asm stub to do
    something reasonable if lang_start returns.

--- lib.rs
+++ lib.rs
@@ -34,7 +34,7 @@ fn lang_start(main: *const u8, _argc: isize, _argv: *const *const u8) -> isize {
     unsafe {
         core::intrinsics::transmute::<_, fn()>(main)();
     }
-    0
+    panic!("Root server shouldn't ever return from main!");
 }
 
 // the initial thread really ought not fail. but if it does, hang.
