193b7da039aece1191812c3962d0b3098c1bb9be
Russell Mull <russell@auxon.io>
2019-05-16 11:36:44 -0700

    Use selfe-arc in example application

---
+++ example_application/build.rs
@@ -0,0 +1,5 @@
+use cargo_5730;
+
+fn main() {
+    cargo_5730::run_build_crate("build-script");
+}
--- example_application/src/main.rs
+++ example_application/src/main.rs
@@ -1,11 +1,17 @@
 #![no_std]
-#![feature(lang_items, core_intrinsics)]
+#![feature(lang_items, core_intrinsics, asm, global_asm)]
 
 use core::panic::PanicInfo;
 
 use core::fmt::Write;
 use sel4_start::{self, DebugOutHandle};
+use selfe_arc;
 use selfe_sys::{seL4_BootInfo, seL4_CapInitThreadTCB, seL4_TCB_Suspend};
+
+extern "C" {
+    static _selfe_arc_data_start: u8;
+    static _selfe_arc_data_end: usize;
+}
 
 fn main() {
     #[cfg(target_arch = "arm")]
@@ -24,6 +30,19 @@ fn main() {
     )
     .unwrap();
 
+    let archive_slice: &[u8] = unsafe {
+        core::slice::from_raw_parts(
+            &_selfe_arc_data_start,
+            &_selfe_arc_data_end as *const _ as usize - &_selfe_arc_data_start as *const _ as usize,
+        )
+    };
+
+    let archive = selfe_arc::read::Archive::from_slice(archive_slice);
+    let data_file_slice = archive.file("data_file.txt").unwrap();
+    let content = core::str::from_utf8(data_file_slice).unwrap();
+
+    writeln!(DebugOutHandle, "Read data file from selfe arc: {}", content).unwrap();
+
     let suspend_error = unsafe { seL4_TCB_Suspend(seL4_CapInitThreadTCB as usize) };
     if suspend_error != 0 {
         writeln!(
@@ -39,3 +58,20 @@ fn main() {
 fn panic(info: &PanicInfo) -> ! {
     sel4_start::debug_panic_handler(&info)
 }
+
+global_asm!(
+    r#"
+.section ".rodata"
+.balign 4096
+
+.global _selfe_arc_data_start
+.type _selfe_arc_data_start, object
+_selfe_arc_data_start:
+  .incbin "target/selfe_arc_data"
+
+.balign 4096
+
+.global _selfe_arc_data_end
+_selfe_arc_data_end:
+"#
+);
