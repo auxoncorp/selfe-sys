d7a64270df5cfda420b86e6d31f7ae8e1569c845
Jon Lamb <jon@auxon.io>
2019-06-25 06:53:27 -0700

    Fix example app build script archiving

--- example_application/build-script/src/main.rs
+++ example_application/build-script/src/main.rs
@@ -2,9 +2,5 @@ use selfe_arc;
 use std::path::Path;
 
 fn main() {
-    selfe_arc::build::link_with_archive(
-        &[
-            ("data_file.txt", &Path::new("../data_file.txt")),
-        ]
-    );
+    selfe_arc::build::link_with_archive(vec![("data_file.txt", Path::new("../data_file.txt"))]);
 }
