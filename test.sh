efbcbd35f95b65206b47e76511bcaab7d97a9072
Zachary Pierce <zack@auxon.io>
2019-06-27 13:22:46 -0700

    Add virt platform to example app

--- test.sh
+++ test.sh
@@ -21,9 +21,11 @@ RUSTFLAGS="-C link-args=-no-pie" cargo test
     SEL4_PLATFORM=sabre cargo xbuild --target armv7-unknown-linux-gnueabihf
     SEL4_PLATFORM=pc99 cargo xbuild --target=x86_64-unknown-linux-gnu
     SEL4_PLATFORM=tx1 cargo xbuild --target aarch64-unknown-linux-gnu
+    SEL4_PLATFORM=virt cargo xbuild --target aarch64-unknown-linux-gnu
 
     ../selfe-config/target/debug/selfe build --sel4_arch x86_64 --platform pc99 --debug
     ../selfe-config/target/debug/selfe build --sel4_arch x86_64 --platform pc99 --release
     ../selfe-config/target/debug/selfe build --sel4_arch aarch32 --platform sabre
+    ../selfe-config/target/debug/selfe build --sel4_arch aarch64 --platform virt
 )
 
