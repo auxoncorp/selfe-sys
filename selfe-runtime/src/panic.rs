1dcb1eaa384693ede5ccb895d6dd286cf85b9532
Russell Mull <russell@auxon.io>
2019-07-31 09:55:39 -0700

    Break out abort function for debuggability

--- selfe-runtime/src/panic.rs
+++ selfe-runtime/src/panic.rs
@@ -3,7 +3,12 @@ use core::fmt::Write;
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
     let _res = writeln!(crate::debug::DebugOutHandle, "*** Panic: {:#?}", info);
+    abort()
+}
 
+/// This is a separate function so there's a clean place to set an abort
+/// breakpoint, for debug builds.
+fn abort() -> ! {
     unsafe {
         core::intrinsics::abort();
     }
