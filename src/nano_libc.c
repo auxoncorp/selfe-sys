3ca48462377c82c27e5153717ddbb479bf5cedb9
Russell Mull <russell@auxon.io>
2019-08-15 17:59:24 -0700

    Move __assert_fail and strcpy into selfe-sys
    Exporting these from rust libraries is unreliable. The previously present
    re-exporting from selfe-runtime to libsel4-start is particularly bad, as the
    ambiguity between the two versions leads to the compiler generating a recursive
    function that eats the stack.
    
    This implements the two functions in plain C, and puts them right next to the
    code that needs them.

---
+++ src/nano_libc.c
@@ -0,0 +1,31 @@
+#ifdef KernelPrinting
+extern void seL4_DebugPutChar(char);
+#endif
+
+extern char * strcpy( char * s1, const char * s2 )
+{
+  char * rc = s1;
+  while ( ( *s1++ = *s2++ ) );
+  return rc;
+}
+
+
+void debug_puts(char* s) {
+  #ifdef KernelPrinting
+  while ( *s != '\0' ) {
+    seL4_DebugPutChar(*s++);
+  }
+  #endif
+}
+
+extern void __assert_fail(char *expr, char* file, int line, char* func) {
+  debug_puts("ASSERT ");
+  debug_puts(expr);
+  debug_puts(" in ");
+  debug_puts(func);
+  debug_puts(" at ");
+  debug_puts(file);
+  debug_puts(":??");
+
+  __builtin_trap();
+}
